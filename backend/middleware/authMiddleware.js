eb660661e9ea0934f42976098544af0d48a6b0b3
rohananwaar <rohananwaar@gmail.com>
2024-02-25 15:06:52 +0500

    Workaround admin User changes in auth User

--- backend/middleware/authMiddleware.js
+++ backend/middleware/authMiddleware.js
@@ -11,7 +11,7 @@ const protect = asyncHandler(async (req, res, next) => {
 
   if (token) {
     try {
-      const decoded = jwt.verify(token, process.env.JWT_SECRET);
+      const decoded = jwt.verify(token, 'abc123def456');
 
       req.user = await User.findById(decoded.userId).select('-password');
 
